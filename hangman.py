f2863beedc0d5064b64efe77c3f052c022e17fbf
albert <albert_dino@live.nl>
2023-03-19 18:51:25 +0100

    hangman final

--- hangman.py
+++ hangman.py
@@ -1,7 +1,5 @@
 import random
-
 quit = False
-
 def readFile(file):
     wordList = []
     with open(file, "r") as f:
@@ -11,60 +9,59 @@ def readFile(file):
 def randomWord(list):
     return random.choice(list)
 
-def takeGuess(guess, guessList):
-    if not guess.isalpha():
-        print("Dit is geen letter!")
-    elif len(guess) > 1 or len(guess) == 0:
-        print("Voer niet meer of minder dan 1 letter in!")
-    #check if the guess is already in guessList
-    elif any(dict["guess"] == guess for dict in guessList):
-        print("Deze letter heb je al geprobeerd!")
-    else:
-        return guess       
-
-def checkGuess(guess, solution, guessList, permittedTries, partialSolution):
-    if guess in solution:
-        guessList.append({"guess":guess, "inSolution":True})
-        #fill in blanks on partialsolution
-        tempPartialSolution = ""
-        for i in range(len(partialSolution)):
-            if partialSolution[i] == "_" and solution[i] == guess:
-                tempPartialSolution += guess
-            else:
-                tempPartialSolution += partialSolution[i]
-        partialSolution = tempPartialSolution
-    else:
-        guessList.append({"guess":guess, "inSolution":False})
-        permittedTries -= 1
-        print(partialSolution)
-        print("Je hebt nog " + str(permittedTries) + " beurten over")
+def takeGuess(guessList):
+    guess = ""
+    while guess == "":
+        userInput = input("Kies een letter: ")
+        if not userInput.isalpha():
+            print("Dit is geen letter!")
+        elif len(userInput) > 1 or len(userInput) == 0:
+            print("Voer niet meer of minder dan 1 letter in!")
+        #check if the guess is already in guessList
+        elif any(dict["guess"] == userInput for dict in guessList):
+            print("Deze letter heb je al geprobeerd!")
+        else:
+            guess = userInput
+    return guess       
 
 def checkSolution(solution, partialSolution):
     if partialSolution == solution:
         print("you win!")  
-        quit = True
     else:
         print("you lose!")
-        quit = True
-    
+
 def play():
+    global quit
     solution = ""
     guessList = []
-    wordList = readFile("Woordlijst.txt")
+    wordList = readFile("C:/Users/alber/Documents/hacklab opdrachten/hacklab-documents/Woordlijst.txt")
     solution = randomWord(wordList)
     partialSolution = ""
+    permittedTries = 10
     for i in solution:
         partialSolution += "_"
     guessList = []
-    permittedTries = 10
 
     while permittedTries != 0 and partialSolution != solution:
-        userInput = input("Kies een letter: ")
-        guess = takeGuess(userInput, guessList)
-        checkGuess(guess, solution, guessList, permittedTries, partialSolution)
+        guess = takeGuess(guessList)
+        if guess in solution:
+            guessList.append({"guess":guess, "inSolution":True})
+            #fill in blanks on partialsolution
+            tempPartialSolution = ""
+            for i in range(len(partialSolution)):
+                if partialSolution[i] == "_" and solution[i] == guess:
+                    tempPartialSolution += guess
+                else:
+                    tempPartialSolution += partialSolution[i]
+            partialSolution = tempPartialSolution
+        else:
+            guessList.append({"guess":guess, "inSolution":False})
+            permittedTries -= 1
+        print(partialSolution)
+        print("Je hebt nog " + str(permittedTries) + " beurten over")        
     checkSolution(solution, partialSolution)
     rematch = ""
-    while rematch != "y" or rematch != "n":
+    while rematch != "y" and rematch != "n":
         rematch = input("Rematch? \"y\" or \"n\"")
     if rematch == "n":
         quit = True
